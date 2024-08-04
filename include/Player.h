de09f430b4a1ca4a670e75bdbc721e8e467dc2ee
WalterINF <walter.frank1059@gmail.com>
2024-08-04 18:30:46 -0300

    added player stats

---
+++ include/Player.h
@@ -0,0 +1,16 @@
+//
+// Created by walte on 04/08/2024.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+struct PlayerStats {
+    float health;
+    float regen_factor;
+    float mana;
+    float mana_regen_factor;
+    float walkspeed;
+};
+
+#endif //PLAYER_H
