fbfc39d539a20966ca4e6aacd80cb838718cfca5
WalterINF <walter.frank1059@gmail.com>
2024-08-09 16:56:27 -0300

    added GameEntity class

---
+++ include/GameEntity.h
@@ -0,0 +1,50 @@
+//
+// Created by walte on 09/08/2024.
+//
+
+#ifndef GAMEENTITY_H
+#define GAMEENTITY_H
+
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+#define STATE_IDLE 0
+#define STATE_ROAMING 1
+#define STATE_CHASING 2
+
+class GameEntity {
+public:
+    GameEntity(glm::vec4 start_pos, int type, float health, float speed, float range);
+
+    void set_health(float health);
+    void set_walkspeed(float walkspeed);
+    void set_range(float range);
+    void set_position(const glm::vec4 &position);
+    void set_direction(const glm::vec4 &direction);
+    void set_target(const glm::vec4 &target);
+    void set_state(int state);
+
+    float get_health() const;
+    float get_walkspeed() const;
+    float get_range() const;
+    glm::vec4 get_position() const;
+    glm::vec4 get_direction() const;
+    glm::vec4 get_target() const;
+    int get_state() const;
+
+    void step(float timeDelta);
+
+private:
+    float health;
+    float walkspeed;
+    float range;
+
+    glm::vec4 position;
+    glm::vec4 direction;
+    glm::vec4 target;
+    int state;
+    int entity_type;
+};
+
+
+#endif //GAMEENTITY_H
---
+++ src/game/GameEntity.cpp
@@ -0,0 +1,88 @@
+//
+// Created by walte on 09/08/2024.
+//
+
+#include "GameEntity.h"
+
+#include <_mingw_stat64.h>
+#include <glm/ext/quaternion_geometric.hpp>
+
+
+GameEntity::GameEntity(glm::vec4 start_pos, int type, float health, float speed, float range) {
+    this->position = start_pos;
+    this->entity_type = type;
+    this->health = health;
+    this->walkspeed = speed;
+    this->range = range;
+}
+
+void GameEntity::set_health(float health) {
+    this->health = health;
+}
+
+void GameEntity::set_walkspeed(float walkspeed) {
+    this->walkspeed = walkspeed;
+}
+
+void GameEntity::set_range(float range) {
+    this->range = range;
+}
+
+void GameEntity::set_position(const glm::vec4 &position) {
+    this->position = position;
+}
+
+void GameEntity::set_direction(const glm::vec4 &direction) {
+    this->direction = direction;
+}
+
+void GameEntity::set_target(const glm::vec4 &target) {
+    this->target = target;
+}
+
+void GameEntity::set_state(int state) {
+    this->state = state;
+}
+
+float GameEntity::get_health() const {
+    return health;
+}
+
+float GameEntity::get_walkspeed() const {
+    return walkspeed;
+}
+
+float GameEntity::get_range() const {
+    return range;
+}
+
+glm::vec4 GameEntity::get_position() const {
+    return position;
+}
+
+glm::vec4 GameEntity::get_direction() const {
+    return direction;
+}
+
+glm::vec4 GameEntity::get_target() const {
+    return target;
+}
+
+int GameEntity::get_state() const {
+    return state;
+}
+
+void GameEntity::step(float timeDelta) {
+    switch (state) {
+        case STATE_IDLE:
+            break;
+        case STATE_ROAMING:
+            break;
+        case STATE_CHASING:
+            direction = target - position;
+            position += glm::normalize(direction) * walkspeed;
+            break;
+        default:
+            break;
+    }
+}
