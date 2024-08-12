e2cbd9c5e717d77048bafde98e783f6b4858ea1c
srsviegas <rudiero.viegas@gmail.com>
2024-08-12 17:32:57 -0300

    FEAT: Add `Sphere` class, `AABB` struct, and `Sphere.IsCollidingAABB` method.

---
+++ include/Collisions.h
@@ -0,0 +1,25 @@
+#ifndef COLLISIONS_H
+#define COLLISIONS_H
+
+#include <glm/glm.hpp>
+
+#include <algorithm> // min and max functions
+
+struct AABB
+{
+    glm::vec3 min;
+    glm::vec3 max;
+};
+
+class Sphere
+{
+public:
+    Sphere(glm::vec3 c, float r);
+    bool IsCollidingAABB(AABB aabb);
+
+private:
+    glm::vec3 center;
+    float radius;
+};
+
+#endif
---
+++ src/core/Collisions.cpp
@@ -0,0 +1,23 @@
+#include "Collisions.h"
+
+/* Creates a Sphere object, where `c` is the center position as a vec3 and `r` is the
+radius as a float value. */
+Sphere::Sphere(glm::vec3 c, float r)
+{
+    center = c;
+    radius = r;
+}
+
+/* Checks collision between the Sphere object and an AABB object. */
+bool Sphere::IsCollidingAABB(AABB aabb)
+{
+    glm::vec3 closest;
+    closest.x = std::max(aabb.min.x, std::min(center.x, aabb.max.x));
+    closest.y = std::max(aabb.min.y, std::min(center.y, aabb.max.y));
+    closest.z = std::max(aabb.min.z, std::min(center.z, aabb.max.z));
+
+    glm::vec3 distance = closest - center;
+    float distanceSquared = glm::dot(distance, distance);
+
+    return distanceSquared <= (radius * radius);
+}
